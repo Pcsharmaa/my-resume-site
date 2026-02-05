d9aa4464007f7472fd116aa9037eaaeb993a42d2
prafull sharma <prafullsharma0524@outlook.com>
2026-02-05 22:58:03 +0530

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/my-resume-site/", 
   server: {
     host: "::",
     port: 8080,
